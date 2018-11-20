f324b4120c51ee99dac61490ac96e54ea616a9dc
mraga13 <45187221+mraga13@users.noreply.github.com>
2018-11-19 20:35:43 -0500

    Create customVariables.scala

---
+++ customVariables.scala
@@ -0,0 +1,11 @@
+package com.delta.svoc
+
+trait customVariables {
+  val customAppName = "CTL-Parse_Raw-Pub"
+  val customSparkMaster = "local[*]"
+  val rawHiveDB = "svoc_ctl"
+  // val curHiveDB = ""
+  // val
+  // val i = 5
+  // val j = 3
+}
